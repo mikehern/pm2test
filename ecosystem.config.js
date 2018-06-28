971bd94cb0c5af0123951b7ab3aaff0a49711088
Mike Hernandez <mkhern@gmail.com>
2018-06-28 15:00:12 -0700

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'i0',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-57-35-120.us-west-1.compute.amazonaws.com',
+      key: '~/60/sandbox/config/iteration0.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:mikehern/pm2test.git',
+      path: '/home/ubuntu/test/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
